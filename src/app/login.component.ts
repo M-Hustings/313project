11c1604fd3dfd05d798175eff6e93c4649a4f79d
M-Hustings <104599217+M-Hustings@users.noreply.github.com>
2022-04-28 17:21:55 -0500

    Add files via upload

---
+++ src/app/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  email: string = '';
+  password: string = '';
+  showError: boolean = false;
+  showSuccess: boolean = false;
+
+  constructor(private accService: AccountService) {}
+
+  ngOnInit(): void {}
+
+  tryLogin(): void {
+    console.log("Attempting login with email: "+this.email.replace(" ","")+" ; password: "+this.password.replace(" ",""));
+    let success = this.accService.login(this.email.replace(" ",""),this.password.replace(" ",""));
+    this.showError = !success;
+    this.showSuccess = success;
+  }
+
+}
